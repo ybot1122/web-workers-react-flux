4fae80eff00e1fb51359157e97a721bb389f6a08
Toby Liu <ybot1122@gmail.com>
2015-11-18 19:18:37 -0800

    Simple dispatcher

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 // main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Dispatcher = require('./Dispatcher.js');
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.body
-);
+var MyStore = Store.create(Dispatcher, {
+  dispatch: function (payload) {
+    switch (payload.type) {
+      case 'updateData':
+        this.data = payload.data;
+        this.flush();
+        break;
+    }
+  }
+});
+
+ReactDOM.render(<h1>Hello, world!</h1>, document.body);
