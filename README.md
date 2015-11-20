4e6b5965d006e6a8e2e15197e280c1ad4ce24191
Toby Liu <ybot1122@gmail.com>
2015-11-19 19:26:28 -0800

    added fake functions to simulate api calls

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Some sample patterns and implementations for implementing dispatchers and stores in Flux architecture
 
 for now, build with:
-browserify -t [ babelify --presets [ react ] ] main.js -o bundle.js
+browserify -t [ babelify --presets [ react ] ] main.js > bundle.js
---
+++ ServiceCalls.js
@@ -0,0 +1,39 @@
+/**
+  Collection of wrappers to make asycnhronous calls to endpoints
+
+  -- for now just a bunch of async routines with artifical delays
+**/
+
+module.exports = {
+  getFakeWeather() {
+    const fakeResult = {
+      location: 'Portland, Oregon',
+      forecast: 'Cloudy with a chance of rain'
+    };
+    return new Promise(function(resolve, reject) {
+      window.setTimeout(function() {
+        resolve(fakeResult);
+      }, 3000);
+    });
+  },
+
+  getFakeNews() {
+    const fakeResult = {
+      headline: 'Something Is Going On In Here',
+      summary: 'Not long has passed since something went on here.'
+    };
+    return new Promise(function(resolve, reject) {
+      window.setTimeout(function() {
+        resolve(fakeResult);
+      }, 1000);
+    });
+  },
+
+  getFakeVideo() {
+    return new Promise(function(resolve, reject) {
+      window.setTimeout(function() {
+        reject('rejected, lets just stay friends');
+      }, 3000);
+    });
+  }
+}
